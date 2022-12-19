46880553f990ce730b2f9124e24a3a22d077271f
Alisa <Alica1200@gmail.com>
2022-12-19 11:38:04 +0300

    Add 4hm

--- src/main/java/ru/gb/oseminar/Main.java
+++ src/main/java/ru/gb/oseminar/Main.java
@@ -1,29 +1,15 @@
 package ru.gb.oseminar;
 
- import ru.gb.oseminar.controller.UserController;
+import ru.gb.oseminar.controller.TaskController;
+import ru.gb.oseminar.model.*;
+import ru.gb.oseminar.view.ViewTask;
 
- public class Main {
-     public static void main(String[] args) {
-         UserController controller = new UserController();
-
-         controller.createTeacher("Aleksandr", "Aleksandrov", "Aleksandrovich");
-         controller.createStudent("Vladislav", "Vladislavov", "Vladislavovich");
-         controller.createStudent("Petr", "Petrov", "Petrovich");
-         controller.createStudent("Ivan", "Ivanov", "Ivanovich");
-         controller.createStudyGroup();
-
-         controller.createTeacher("Anton", "Antonov", "Antonovich");
-         controller.createStudent("Vladimir", "Konstantinov", "Vladimirovich");
-         controller.createStudent("Konstantin", "Sokolov", "Konstantinovich");
-         controller.createStudent("Evgeniy", "Evgen'ev", "Evgenevich");
-         controller.createStudyGroup();
-
-         controller.createTeacher("Aleksey", "Alekseev", "Alekseevich");
-         controller.createStudent("Yuriy", "Yur'ev", "Yur'evich");
-         controller.createStudent("Maksim", "Maksimov", "Maksimovich");
-         controller.createStudent("Boris", "Borisov", "Borisovich");
-         controller.createStudyGroup();
-
-         controller.showSortedStudents(controller.getAllStudyGroups());
-     }
- }
+public class Main {
+    public static void main(String[] args) {
+        FileOperation fileOperation = new FileOperationImpl("tasks.csv");
+        Repository repository = new RepositoryFile(fileOperation);
+        TaskController controller = new TaskController(repository);
+        ViewTask view = new ViewTask(controller);
+        view.run();
+    }
+}
